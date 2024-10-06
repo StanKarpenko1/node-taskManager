86922cd6ae368a12b21c886a2529a5a28bfa6dad
staskarpenko1 <karpenko.stas@gmail.com>
2024-10-05 23:33:14 -0400

    misc

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -7,13 +7,13 @@ const createTask = (req: Request, res: Response) => {
     res.json(req.body) 
 };
 const getTask = (req: Request, res: Response) => {
-    res.send({ id: req.params.id }) 
+    res.json({ id: req.params.id }) 
 };
 const deleteTask = (req: Request, res: Response) => {
-    res.send('Delete Task') 
+    res.json({ deleted: req.params.id }) 
 };
 const updateTask = (req: Request, res: Response) => {
-    res.send('Update Task') 
+    res.json({updated: req.params.id }) 
 };
 
 module.exports = {
